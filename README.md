b4256a0fcbf036d37b1280436574eb9349bf5c54
Aaron Steele <eightysteele@gmail.com>
2012-04-21 09:14:46 -0700

    Initial commit of API.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-api
-===
+# API
+
+# WebGL API
+
+This API surfaces data for use in the WebGL visualization. 
+
+Results are returned in JSON. The general format is:
+
+```json
+[key, [[lat, lon, magnitude], ..., [lat, lon, magnitude]]
+```
+
+Example request for 10 fires with magnitude coming from the brightness column:
+
+[http://ecohackfirez.appspot.com/api/webgl?count=10&mag=brightness](http://ecohackfirez.appspot.com/api/webgl?count=10&mag=brightness)
+
+Description of URL parameters:
+
+<table>
+    <tr>
+        <td><b>Parameter</b></td>
+        <td><b>Description</b></td>
+        <td><b>Example</b></td>
+    </tr>
+    <tr>
+        <td>count</td>
+        <td>Number of fires to return</td>
+        <td10</td>
+    </tr>
+    <tr>
+        <td>mag</td>
+        <td>Magnitude of fire (brightness or confidence)</td>
+        <td>confidence</td>
+    </tr>
+</table>
+
---
+++ api.py
@@ -0,0 +1,53 @@
+from google.appengine.ext.webapp.util import run_wsgi_app
+"""API proxy to CartoDB.
+
+"""
+
+import json
+import logging
+import os
+import random
+import urllib
+import webapp2
+
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
+
+class ApiHandler(webapp2.RequestHandler):
+    def get(self):
+        """Handler for WebGL requests returning GEOJSON.
+        
+        /api/webgl?mag=b&count=10
+
+        Example:
+          [["2012", [28.915, 19.781, 306.5]], ["2011", [21.864,98.677, 319.8]], ["2010", [11.105, -6.337, 332.8]]]
+        """
+        mag = self.request.get('mag', 'brightness')
+        count = self.request.get_range('count', default=10)
+        
+        key = 'mag=%s,count=%s' % (mag, count)
+        value = memcache.get(key)
+
+        if not value:
+            sql = "SELECT lat, lon, %s, date FROM fires limit %s" % (mag, count)
+            url = 'http://ecohackfirez.cartodb.com/api/v2/sql?%s' % urllib.urlencode(dict(q=sql))
+            content = json.loads(urlfetch.fetch(url, deadline=60).content) # TODO: retry
+            if not content.has_key('error'):
+                value = json.dumps(map(lambda row: [row['date'], [row['lat'], row['lon'], row[mag]]], content['rows']))
+                memcache.add(key, value)
+        
+        if not value:
+            value = []
+
+        self.response.headers["Content-Type"] = "application/json"
+        self.response.out.write(value)
+
+application = webapp2.WSGIApplication(
+         [('/api/webgl', ApiHandler)],
+         debug=True
+)
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
